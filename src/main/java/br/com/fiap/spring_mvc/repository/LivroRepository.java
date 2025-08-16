34776c4b0373bf19243ec2b9f2a774ad0d30f836
vinibanciela <vinibanciela@gmail.com>
2025-08-15 21:59:32 -0300

    livroCadastro html, livroLista html

--- src/main/java/br/com/fiap/spring_mvc/repository/LivroRepository.java
+++ src/main/java/br/com/fiap/spring_mvc/repository/LivroRepository.java
@@ -4,6 +4,7 @@ import br.com.fiap.spring_mvc.entity.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface LivroRepository extends JpaRepository<Livro, Long> {
 }
