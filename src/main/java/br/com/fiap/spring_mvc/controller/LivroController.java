602d98f8854f1a99b432a46b4ecb97458d2f3e70
vinibanciela <vinibanciela@gmail.com>
2025-08-19 22:20:28 -0300

    aula 18.08.25

--- src/main/java/br/com/fiap/spring_mvc/controller/LivroController.java
+++ src/main/java/br/com/fiap/spring_mvc/controller/LivroController.java
@@ -1,29 +1,41 @@
 package br.com.fiap.spring_mvc.controller;
 
+import br.com.fiap.spring_mvc.entity.Categoria;
 import br.com.fiap.spring_mvc.entity.Livro;
+import br.com.fiap.spring_mvc.service.LivroService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
 @RequestMapping("/livros")
 public class LivroController {
+    @Autowired
+    LivroService livroService;
+
     @GetMapping("/lista")
     public String listarLivros(Model model) {
-        Livro livro1 = new Livro();
-        livro1.setId(1L);
-        livro1.setTitulo("Harry Potter");
-        Livro livro2 = new Livro();
-        livro2.setId(2L);
-        livro2.setTitulo("Percy Jackson");
-        Livro livro3 = new Livro();
-        livro3.setId(3L);
-        livro3.setTitulo("O diário de um banana");
-        List<Livro> livros = List.of(livro1, livro2, livro3);
+        List<Livro> livros = livroService.readLivros();
         model.addAttribute("listaLivros", livros);
+        return "livroLista";
+    }
+
+    @GetMapping("/cadastro")
+    public String cadastroLivro(Model model) {
+        model.addAttribute("livro", new Livro());
+        model.addAttribute("categoriaLista", Arrays.asList(Categoria.values()));
+        return "livroCadastro";
+    }
+
+    @PostMapping("/cadastrar")
+    public String cadastrarLivro(Livro livro, Model model) {
+        livroService.createLivro(livro);
         return "livroLista";
     }
 }
--- src/main/java/br/com/fiap/spring_mvc/repository/LivroRepository.java
+++ src/main/java/br/com/fiap/spring_mvc/repository/LivroRepository.java
@@ -4,7 +4,6 @@ import br.com.fiap.spring_mvc.entity.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
 public interface LivroRepository extends JpaRepository<Livro, Long> {
 }
