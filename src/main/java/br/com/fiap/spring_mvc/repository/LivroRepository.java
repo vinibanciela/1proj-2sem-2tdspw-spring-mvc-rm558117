ccf96b4f6886d958b62337ec5772a2444035069a
vinibanciela <vinibanciela@gmail.com>
2025-09-12 21:57:23 -0300

    removendo arquivos duplicados e desncessários

--- src/main/java/br/com/fiap/spring_mvc/repository/LivroRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.spring_mvc.repository;
-
-import br.com.fiap.spring_mvc.entity.Livro;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LivroRepository extends JpaRepository<Livro, Long> {
-}
