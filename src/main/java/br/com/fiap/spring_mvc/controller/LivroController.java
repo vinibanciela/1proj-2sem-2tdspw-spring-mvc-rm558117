965f55132ca8b7e6682ab94ef8e779d3714ffb61
vinibanciela <vinibanciela@gmail.com>
2025-09-12 21:53:44 -0300

    aula 12.09.25

--- src/main/java/br/com/fiap/spring_mvc/controller/LivroController.java
+++ src/main/java/br/com/fiap/spring_mvc/controller/LivroController.java
@@ -3,10 +3,13 @@ package br.com.fiap.spring_mvc.controller;
 import br.com.fiap.spring_mvc.entity.Categoria;
 import br.com.fiap.spring_mvc.entity.Livro;
 import br.com.fiap.spring_mvc.service.LivroService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -34,8 +37,34 @@ public class LivroController {
     }
 
     @PostMapping("/cadastrar")
-    public String cadastrarLivro(Livro livro, Model model) {
-        livroService.createLivro(livro);
-        return "livroLista";
+    public String cadastrarLivro(@Valid Livro livro, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("livro", livro);
+            model.addAttribute("categoriaLista", Arrays.asList(Categoria.values()));
+            return "livroCadastro";
+        }
+        if (livro.getId() == null) {
+            livroService.createLivro(livro);
+        } else {
+            livroService.updateLivro(livro);
+        }
+        return listarLivros(model);
+    }
+
+    @GetMapping("/cadastro/{id}")
+    public String cadastroLivro(@PathVariable Long id, Model model) {
+        Livro livro = livroService.readLivro(id);
+        if (livro == null) {
+            listarLivros(model);
+        }
+        model.addAttribute("livro", livro);
+        model.addAttribute("categoriaLista", Arrays.asList(Categoria.values()));
+        return "livroCadastro";
+    }
+
+    @GetMapping("/deletar/{id}")
+    public String deletarLivro(@PathVariable Long id, Model model) {
+        livroService.deleteLivro(id);
+        return listarLivros(model);
     }
 }
